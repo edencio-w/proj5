1a304914fe8ea12fd09985e6f1d10644dab09ce0
edencio-w <55332130+edencio-w@users.noreply.github.com>
2019-11-23 15:08:45 -0500

    fixed_includes_and_comments

--- Functions.cpp
+++ Functions.cpp
@@ -3,10 +3,9 @@
  * Functions.cpp
  *
  *  Created on: Nov 5, 2017
- *      Author: keith
+ *      Author: Edencio Wilcher + keith
  */
 #include <iostream>
-#include <memory>
 #include <string>
 #include "./includes/Functions.h"
 #include "./includes/Smalltalk_American.h"
@@ -14,25 +13,20 @@
 #include "./includes/Smalltalk_Brit.h"
 #include "./includes/Watch.h"
 #include "./includes/constants.h"
-#include <vector>
 
 std::vector<std::unique_ptr<Smalltalk>> getPeople(int numBrit, int numAmerican,
 		int numbAmericanDonutEnthusiest, int numWatches) {
 
-	//create a vector to hold SmallTalk unique pointers
 	std::vector<std::unique_ptr<Smalltalk>> vect;
 
-	//add brits to vector
 	for (int i = 0; i < numBrit; i++) {
 		vect.emplace_back(new Smalltalk_Brit(i));
 	}
 
-	//add americans  to vector
 	for (int i = 0; i < numAmerican; i++) {
 		vect.emplace_back(new Smalltalk_American(i));
 	}
 
-	//add american donut enthusiest  to vector
 	for (int i = 0; i < numbAmericanDonutEnthusiest; i++) {
 		vect.emplace_back(new ST_American_DonutEnthusiest(i));
 	}
